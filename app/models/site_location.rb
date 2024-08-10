649440cc302a86fe0e4e58e445488c9d88ec45fa
Mike Wenger <michaelwenger27@gmail.com>
2024-08-09 20:52:56 -0400

    Add ActiveAdmin: Generated with claude-dev
    Prompt:
    Add the ActiveAdmin gem with an admin user that can take CRUD actions on
    the SiteOrganization and SiteLocation
    
    Cost:
    $0.54

--- app/models/site_location.rb
+++ app/models/site_location.rb
@@ -1,3 +1,11 @@
 class SiteLocation < ApplicationRecord
   belongs_to :site_organization
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "created_at", "id", "name", "site_organization_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["site_organization"]
+  end
 end
